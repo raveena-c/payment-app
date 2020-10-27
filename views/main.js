69b0c4b57fb33b17ea4231f0e813e1e23a579595
Chawla <raveena.chawla@accenture.com>
2020-10-27 14:10:04 +0530

    payment-app

--- views/main.js
+++ views/main.js
@@ -53,7 +53,7 @@ $(document).ready(function(){
           }});
 
           function checkout(token,billingAddress,customer,amount,uuid) {
-              public_api_key=process.env.public_key;
+            public_api_key="cHVibGljLTc3NTE6Qi1xYTItMC01ZjAzMWNiZS0wLTMwMmQwMjE1MDA4OTBlZjI2MjI5NjU2M2FjY2QxY2I0YWFiNzkwMzIzZDJmZDU3MGQzMDIxNDUxMGJjZGFjZGFhNGYwM2Y1OTQ3N2VlZjEzZjJhZjVhZDEzZTMwNDQ=";
             paysafe.checkout.setup(public_api_key, {
                 "currency": "USD",
                 "amount": parseInt(amount)*100,
